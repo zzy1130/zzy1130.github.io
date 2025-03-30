39d854257d8cad78604e883b439db139f609fa07
zzy1130 <zzhi20021130@gmail.com>
2025-03-30 17:49:29 +0800

    npm build and deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,3 +16,9 @@ export default defineConfig({
     },
   },
 })
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/frontend/'  // 替换为你的GitHub仓库名
+//     : '/'
+// }

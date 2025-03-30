285efe7b4b0c548f743ba81a6e92fbf565229e66
zzy1130 <zzhi20021130@gmail.com>
2025-03-30 18:34:03 +0800

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/frontend/",
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,9 +17,3 @@ export default defineConfig({
     },
   },
 })
-
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/frontend/'  // 替换为你的GitHub仓库名
-//     : '/'
-// }
--- frontend/vue.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/frontend/'   // 注意：必须以斜杠开头和结尾
-    : '/'
-}

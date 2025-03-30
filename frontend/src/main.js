9fa61639a1b07add3619a1492a307ca98d59d66e
zzy1130 <zzhi20021130@gmail.com>
2025-03-30 18:43:11 +0800

    change

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,3 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory('/frontend/'), // 关键配置
+    routes
+  })
+  
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/frontend/",
+  base: '/frontend/', // 必须和 GitHub 仓库名一致
+  build: {
+    outDir: '../docs', // 构建到仓库根目录的 docs 文件夹
+  },
   plugins: [
     vue(),
     vueDevTools(),
